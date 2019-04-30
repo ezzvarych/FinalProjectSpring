e51b91ab3e7a307f2930c087503af307fa34cb05
Yevhen Zvarych <Yevhen_Zvarych@epam.com>
2019-04-30 17:14:04 +0300

    ...

--- src/main/java/com/example/demo/controller/restController/RequestRestController.java
+++ src/main/java/com/example/demo/controller/restController/RequestRestController.java
@@ -2,14 +2,20 @@ package com.example.demo.controller.restController;
 
 import com.example.demo.controller.viewsContoller.UserSupportUtils;
 import com.example.demo.model.entity.request.Request;
+import com.example.demo.model.entity.user.Role;
+import com.example.demo.model.entity.user.User;
 import com.example.demo.model.service.RequestService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-//TODO Finish this controller
+import java.util.List;
+
+//TODO Secure methods(one user cannot get requests of another)
 @RestController
-@RequestMapping("/requests")
+@RequestMapping("/customer/requests")
+@PreAuthorize("hasAuthority('CUSTOMER')")
 public class RequestRestController {
 
     private RequestService requestService;
@@ -19,15 +25,25 @@ public class RequestRestController {
     }
 
     @PostMapping
-    public ResponseEntity<Request> createRequest(String description) {
+    public ResponseEntity<Request> createRequest(@RequestBody String description) {
         Request request = new Request(description, UserSupportUtils.getCurrentUser());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(requestService.create(request));
     }
 
-//    @GetMapping("/unhandled/{id}")
-//    public ResponseEntity<Request> getCustomerUnhandled(@PathVariable long id) {
-//
-//    }
+    @GetMapping("/")
+    public ResponseEntity<List<Request>> getRequests() {
+        return ResponseEntity.ok(requestService.getAllByCustomer(UserSupportUtils.getCurrentUser()));
+    }
+
+    @GetMapping("/unhandled")
+    public ResponseEntity<List<Request>> getCustomerUnhandled() {
+        return ResponseEntity.ok(requestService.getUnhandledByCustomer(UserSupportUtils.getCurrentUser()));
+    }
+
+    @GetMapping("/denied")
+    public ResponseEntity<List<Request>> getCustomerDenied() {
+        return ResponseEntity.ok(requestService.getUnhandledByCustomer(UserSupportUtils.getCurrentUser()));
+    }
 
 }
